635a1c07279fc3968199c5b4d6f52c8487f7d656
BlueIce57 <104216206+BlueIce57@users.noreply.github.com>
2022-07-07 12:57:54 +0200

    Add files via upload

---
+++ src/main/java/fr/blueice/omegice/launcher/ui/panels/pages/content/ContentPanel.java
@@ -0,0 +1,16 @@
+package fr.blueice.omegice.launcher.ui.panels.pages.content;
+
+import fr.blueice.omegice.launcher.ui.panel.Panel;
+import javafx.animation.FadeTransition;
+import javafx.util.Duration;
+
+public abstract class ContentPanel extends Panel {
+    @Override
+    public void onShow() {
+        FadeTransition transition = new FadeTransition(Duration.millis(250), this.layout);
+        transition.setFromValue(0);
+        transition.setToValue(1);
+        transition.setAutoReverse(true);
+        transition.play();
+    }
+}
